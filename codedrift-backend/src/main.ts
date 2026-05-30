94abb030b700519f9d52c6d00082cbba672c971b
Jamius Siam <j@sii.am>
2026-05-30 18:58:11 +0600

    Added Fastify support and configured Prettier

--- codedrift-backend/src/main.ts
+++ codedrift-backend/src/main.ts
@@ -1,5 +1,23 @@
 /*
-* Author: Jamius Siam
-* Since: 30/05/2026
-*/
+ * Author: Jamius Siam
+ * Since: 30/05/2026
+ */
 import "dotenv/config";
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler(request, reply) {
+  return { hello: "world" };
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
