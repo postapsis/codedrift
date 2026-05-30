a91bb177b124a202eada5dabc68682713c93aacd
Jamius Siam <j@sii.am>
2026-05-30 19:59:01 +0600

    Added Tanstack Query

--- codedrift-frontend/src/main.tsx
+++ codedrift-frontend/src/main.tsx
@@ -1,13 +1,19 @@
-import {StrictMode} from "react";
+/*
+ * Author: Jamius Siam
+ * Since: 30/05/2026
+ */
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import {RouterProvider, createRouter} from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "@/main.css";
 
 // Import the generated route tree
-import {routeTree} from "@/routeTree.gen";
+import { routeTree } from "@/routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({routeTree});
+const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -21,6 +27,8 @@ const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
