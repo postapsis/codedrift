4b4974dcc8bac9828c00b7cd85cbb9d8676fa2df
Jamius Siam <j@sii.am>
2026-05-30 17:52:09 +0600

    Init React frontend

---
+++ codedrift-frontend/src/routes/index.tsx
@@ -0,0 +1,18 @@
+/*
+* Author: Jamius Siam
+* Since: 30/05/2026
+*/
+import {createFileRoute} from '@tanstack/react-router'
+import type {JSX} from "react";
+
+const Index = (): JSX.Element => {
+  return (
+    <div className="p-2">
+      <h3>Welcome to Codedrift!</h3>
+    </div>
+  )
+}
+
+export const Route = createFileRoute('/')({
+  component: Index,
+})
